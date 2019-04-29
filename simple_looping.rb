ef4f72ca06f9457e9368147bb30e429420d58bd3
rudi hinds <rudihinds@gmail.com>
2019-04-29 21:39:57 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,8 +1,8 @@
 def loop_iterator(number_of_times)
   # code your solution here using the "loop" keyword
-  z = 0
+  num = 0
   loop do
-    z+=1
+    num+=1
     puts "Welcome to Flatiron School's Web Development Course!"
     break if z == number_of_times
   end
@@ -17,27 +17,27 @@ end
 
 def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
-  a=0
-  while a < 7
+  num=0
+  while num < 7
     puts "Welcome to Flatiron School's Web Development Course!"
-    a+=1
+    num+=1
   end
 end
 
 def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
-  b=7
-  until b == 0
+  num=7
+  until num == 0
     puts "Welcome to Flatiron School's Web Development Course!"
-    b-=1
+    num-=1
   end
 end
 
 def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
-  c=7
-  for c in 1..7 do
+  num=7
+  for num in 1..7 do
     puts "Welcome to Flatiron School's Web Development Course!"
-    c-=1
+    num-=1
   end
 end
